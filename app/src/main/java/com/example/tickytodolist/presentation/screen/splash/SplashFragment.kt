a789a907fb18dc6a44ce54d0f52e4daaec9556ea
shalva96 <gmshalva@gmail.com>
2024-01-15 17:53:24 +0400

    update

---
+++ app/src/main/java/com/example/tickytodolist/presentation/screen/splash/SplashFragment.kt
@@ -0,0 +1,22 @@
+package com.example.tickytodolist.presentation.screen.splash
+
+import com.example.tickytodolist.databinding.FragmentSplashBinding
+import com.example.tickytodolist.presentation.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class SplashFragment : BaseFragment<FragmentSplashBinding>(FragmentSplashBinding::inflate) {
+    override fun bind() {
+
+    }
+
+    override fun bindViewActionListeners() {
+
+    }
+
+    override fun bindObserves() {
+
+    }
+
+}
